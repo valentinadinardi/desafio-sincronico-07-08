50ad52d91b4c8a6ed8e6efc7cc1c4af729f2bf8d
Eugenio Gallo <e.gallo@outlook.com>
2023-08-09 17:33:22 -0300

    Explicando el funcionamiento de la función fetch

--- js/script.js
+++ js/script.js
@@ -29,3 +29,32 @@ fetch(DATA_URL)
   .catch((error) => {
     console.error("Ocurrió un error: ", error);
   });
+
+  // Explicación de la función fetch():
+  
+//   Imagina que estás pidiendo un paquete (datos) a una tienda en línea (servidor) a través de un mensajero (función fetch()). El mensajero va a la tienda y trae de vuelta el paquete.
+
+// Pedir el paquete (solicitud):
+
+// Usas la función fetch() para decirle al mensajero que vaya a la tienda (URL) y traiga algo.
+// La tienda recibe la solicitud y se prepara para enviarte el paquete.
+// Recibir el paquete (respuesta):
+
+// El mensajero vuelve de la tienda con un paquete (respuesta del servidor).
+// Esta respuesta tiene cosas como la información de la tienda (encabezados) y el estado de la entrega (éxito o fallo).
+// Abrir el paquete (convertir a JSON):
+
+// Quieres ver el contenido real del paquete, pero está en un formato especial (encapsulado). Entonces, abres el paquete y ves su contenido (datos) en formato JSON.
+// Convertir la respuesta en JSON es como abrir el paquete y sacar los artículos que hay adentro.
+// Mirar lo que hay dentro (usar los datos):
+
+// Una vez que tienes los artículos del paquete (datos en formato JSON), ves que hay cosas como nombres y apellidos de estudiantes en el paquete.
+// Usas estos datos para decirle a la gente quiénes son los estudiantes y cómo se llaman.
+// Manejar problemas (errores):
+
+// A veces, el mensajero puede tropezar o el paquete puede estar dañado. Si eso sucede, le dices al mensajero que te lo diga (capturar el error) para que puedas entender lo que salió mal.
+// En tu código, la función fetch() es el mensajero que va a buscar los datos a la tienda (servidor), convierte esos datos en JSON para que puedas usarlos, los pasas a la función showData() para mostrar los nombres y apellidos de los estudiantes en una página web y, si algo sale mal, lo atrapas para entender qué pasó.
+
+
+
+
