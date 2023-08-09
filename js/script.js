713174f314a99e7fd58c4266e4460415eedf17ea
Eugenio Gallo <e.gallo@outlook.com>
2023-08-09 17:28:47 -0300

    Creando una función fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    showData(data.students);
+  })
+  .catch((error) => {
+    console.error("Ocurrió un error: ", error);
+  });
